1d40c095e12b1b196d7c03909750ac45fa659c4b
kartik <kabakshi@hotmail.ca>
2021-11-03 18:52:23 -0400

    input

--- input.js
+++ input.js
@@ -1,11 +1,16 @@
 // setup interface to handle user input from stdin
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
+  
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
   stdin.on("data", handleUserInput); // handling user input
+  
   return stdin;
 };
 
@@ -16,6 +21,7 @@ const handleUserInput = function (key) {
   }
   else if (key === 'w'){
   connection.write('Move: up');
+
 } else if ( key === 'a'){
   connection.write('Move: left');
   
@@ -28,6 +34,6 @@ const handleUserInput = function (key) {
 }
 
 };
-setupInput()
+
 
 module.exports ={ setupInput }
