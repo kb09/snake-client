4d20148802dbcd3c2821495fef16d80d7226a070
kartik <kabakshi@hotmail.ca>
2021-11-03 14:13:20 -0400

    client.js

--- client.js
+++ client.js
@@ -9,6 +9,7 @@ const connect = function () {
 
   conn.on('connect',function(){
     console.log('Successfully connected to game server');
+    conn.write('Name: 007')
   })
 
   // interpret incoming data as text
