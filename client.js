4e01ab996a3d1edff3a5d2a7c7e19278715d08cf
kartik <kabakshi@hotmail.ca>
2021-11-03 20:57:00 -0400

    client

--- client.js
+++ client.js
@@ -1,19 +1,25 @@
+//√√
 const net = require("net");
+const { IP, PORT } = require("./constants");
+
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
+    host: IP,
+    port: PORT
   });
 
   conn.on('connect',function(){
     console.log('Successfully connected to game server'); // when connected to server
     conn.write('Name: 007') // name of snake
-  })
+  });
 
 // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    console.log('server message:', data);
+  })
+  return conn;
+}; // end function 
 
-} // end function 
-
-module.exports={ connect }
+module.exports = { connect }
